7d485871ad3ff212f6a3df349512efc6b37c5660
Jack Phillips <jackphillips1313@gmail.com>
2025-08-04 00:07:24 +0100

    Added Player and shape drawing

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import pygame
+from player import Player
 from constants import *
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f'Screen height: {SCREEN_HEIGHT}')
